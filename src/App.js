eb355f4fc04596ab15e542d9ebd7f12f1ba87ac8
Jose Annunziato <jannunzi@gmail.com>
2019-02-06 21:54:00 -0500

    add router to admin screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Admin from './components/Admin'
 
 class App extends Component {
   render() {
     return (
       <div className="container-fluid">
         <h1>ServicesRus</h1>
+        <Router>
+          <div>
+            <Link to="/admin">Admin</Link>
+            <Route
+                path="/admin"
+                exact
+                component={Admin}/>
+          </div>
+        </Router>
       </div>
     );
   }
---
+++ src/components/Admin.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Admin = () =>
+<div>
+    <h2>Admin</h2>
+</div>
+
+export default Admin
