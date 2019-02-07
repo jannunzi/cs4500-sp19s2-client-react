804770e9ee4e1fabc9336ee3004301c9e600646f
Jose Annunziato <jannunzi@gmail.com>
2019-02-06 21:49:08 -0500

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
+      <div className="container-fluid">
+        <h1>ServicesRus</h1>
       </div>
     );
   }
