35ce66b479c492a75667415e7993e2b3415c6633
Jose Annunziato <jannunzi@gmail.com>
2019-02-06 22:16:45 -0500

    retrieved user service and initialized users empty array state

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,6 +3,10 @@ import UserService from '../services/UserService'
 class Users extends React.Component {
     constructor(props) {
         super(props)
+        this.userService = UserService.getInstance()
+        this.state = {
+            users: []
+        }
     }
     render() {
         return(
