9957144bddaa66d75bc4e454151861ea0730a20b
Jose Annunziato <jannunzi@gmail.com>
2019-02-06 22:28:28 -0500

    load all services on mount

--- src/components/ServiceDetails.js
+++ src/components/ServiceDetails.js
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import ServiceService from '../services/ServiceService'
 class ServiceDetails extends React.Component {
     constructor(props) {
         super(props)
+        this.serviceService = ServiceService.getInstance()
+        this.state = {
+            services: []
+        }
+    }
+    componentDidMount() {
+        this.serviceService
+            .findAllServices()
+            .then(services =>
+                this.setState({
+                    services: services
+                })
+            )
     }
     render() {
         return(
